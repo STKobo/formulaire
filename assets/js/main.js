8b55d0af59fa4b2b5ce116f25af712ab6e6bdf48
Stephane <stephanetcha@gmail.com>
2022-08-09 17:33:57 +0200

    Validation email

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,13 +31,51 @@
 
 
 
-            function validateFields(input) {
-                let fieldName = input.name;
-                // Validaton de l'input PRENOM
-                if (fieldName == "firstName") {
-                    if (!validateRequired(input)) {
-                        return false;
-                    }
-                    return (true);
+//Validation du nombre de caractère : MIN & MAX
+function validateLength(input, minLength, maxLength){
+    return !(input.value.length < minLength || input.value.length > maxLength);
+}
+
+//Validation des caractères : LATIN & LETTRES
+function validateText(input){
+    return input.value.match("^[A-Za-z]+$");
+}
+
+//Validation d'un e'mail
+function validateEmail(input){
+    let EMAIL = input.value;
+    let POSAT = EMAIL.indexOf("@");
+    let POSDOT = EMAIL.lastIndexOf(".");
+
+    return !(POSAT < 1 || (POSDOT - POSAT < 2));
+}
+
+
+function validateFields(input) {
+    let fieldName = input.name;
+    // Validaton de l'input PRENOM
+        if (fieldName == "firstName") {
+            if (!validateRequired(input)) {
+                return false;
                 }
+            if (!validateLength(input, 2, 20)){
+                return false;
             }
+            if (!validateText(input)){
+                return false
+            }
+
+                return (true);
+            }
+        
+    //Validation de l'input email    
+        if (fieldName == "email"){
+            if (!validateEmail(input)){
+                return false
+            }
+            return(true);   
+        }
+            
+
+            }
+
