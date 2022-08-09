db8b6d63d5b9658b0a35a864cbc18c42cbbb345f
Stephane <stephanetcha@gmail.com>
2022-08-09 15:31:21 +0200

    Declaration du js

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+   
+    (function() {
+        'use strict'
+        let form = document.getElementById('lessonForm');
+        form.addEventListener('submit', function(event) {
+            
+            Array.from(form.elements).forEach((input) => {
+                if (input.type !== "submit") {
+                    if (!validateFields(input)) {
+                        
+                        event.preventDefault();
+                        event.stopPropagation();
+                        
+                        input.classList.remove("is-valid");
+                        input.classList.add("is-invalid");
+                        input.nextElementSibling.style.display = 'block';
+                    } 
+                    else {
+                        input.nextElementSibling.style.display = 'none';
+                        input.classList.remove("is-invalid");
+                        input.classList.add("is-valid");
+                    }
+                }
+            });
+        }, false)
+    })()
+
